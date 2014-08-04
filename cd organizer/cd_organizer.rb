818f5306f2b954194433c5e2493fc52b0c39b4d4
Lee Kebler & Anna Valkov <student@epicodus.com>
2014-08-04 11:12:00 -0700

    add the ability to search through the albums added to the artists

--- cd organizer/cd_organizer.rb
+++ cd organizer/cd_organizer.rb
@@ -12,9 +12,11 @@ def main_menu
     add_artist
     puts "artist add"
     list_artist
-
+    puts "Enter the number of the artist to enter and album name"
+    artist_number = gets.chomp.to_i
+    add_title(artist_number)
   elsif user_choice == 's'
-    search_cd
+    search
   elsif user_choice == 'x'
     exit
     puts "you're leaving me for an mp3 player aren't you?!"
@@ -28,12 +30,30 @@ end
     puts "Enter the name of the Artist:"
     artist_name = gets.chomp
     Artist.new(artist_name)
+
+  end
+
+  def add_title(number)
+    puts "Enter the name of the album"
+    name = gets.chomp
+    Artist.list_organizer[number -1].add_album(name)
+    puts name + " - album added"
+    main_menu
   end
 
   def list_artist
     puts "Here are all of your current artists"
     Artist.list_organizer.each_with_index do |artist, index|
       puts (index + 1).to_s + " " + artist.name
+    end
+  end
+
+  def search
+    list_artist
+    puts "which artist would you like to search through? select by number:"
+    search_selection = gets.chomp.to_i
+    Artist.list_organizer[search_selection -1].albums.each do |album|
+      puts album
     end
   end
 main_menu
